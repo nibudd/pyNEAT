6b25fcd8ce133ed7502e586b78aa8a7f9016e9e5
Nathan Budd <nibudd@gmail.com>
2025-12-06 14:58:01 -0500

    update EdgeGene

--- EdgeGene.py
+++ EdgeGene.py
@@ -2,10 +2,18 @@ from NodeGene import NodeGene
 
 
 class EdgeGene:
-
-    def __init__(self, in_node: NodeGene, out_node: NodeGene, weight: float, enabled: bool, edge_id: int):
+    def __init__(
+        self,
+        id: int,
+        innovation_id: int,
+        in_node: NodeGene,
+        out_node: NodeGene,
+        weight: float,
+        enabled: bool,
+    ):
+        self.id = id
+        self.innovation_id = innovation_id
         self.in_node = in_node
         self.out_node = out_node
         self.weight = weight
         self.enabled = enabled
-        self.edge_id = edge_id
