d3aaadfcb1958232353dc1a023ae6e07e8ff87e8
Nathan Budd <nibudd@gmail.com>
2025-12-06 15:02:50 -0500

    format Genotype

--- Genotype.py
+++ Genotype.py
@@ -3,7 +3,6 @@ from NodeGene import NodeGene
 
 
 class Genotype:
-
     def __init__(self, node_genes: list[NodeGene], edge_genes: list[EdgeGene]):
         self.edge_genes = edge_genes
         self.node_genes = node_genes
